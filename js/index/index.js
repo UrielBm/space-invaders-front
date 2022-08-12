bd64df6dc4f9d3f8359cfff2cf0089cb091a364c
UrielBm <urielbenitez96@hotmail.com>
2022-08-12 11:38:27 -0500

    new listenner socket event start

--- js/index/index.js
+++ js/index/index.js
@@ -1,6 +1,12 @@
+//variables del Qr y del socket
+const socket = io("http://localhost:3000/");
 let url = window.location.href;
 const sessionGame = Math.random().toString(16).slice(2);
 sessionStorage.setItem("sessionGame", sessionGame);
+console.log(`${url}controller.html?id=${sessionGame}`);
+
+//creación del código Qr
+
 const qrcode = new QRCode(document.getElementById("qrcode"), {
   text: `${url}controller.html?id=${sessionGame}`,
   width: 150,
@@ -9,3 +15,11 @@ const qrcode = new QRCode(document.getElementById("qrcode"), {
   colorLight: "#ffffff",
   correctLevel: QRCode.CorrectLevel.H,
 });
+
+// funciones que se ejecutan al cargar la página
+window.onload = () => {
+  socket.emit("session", { sessionGame });
+  socket.on("start", () => {
+    window.location.href = `${url}game.html`;
+  });
+};
